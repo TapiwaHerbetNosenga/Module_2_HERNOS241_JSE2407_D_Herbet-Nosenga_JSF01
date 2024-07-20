61bc7a76d27a254ffab76951e0fb4afed295c26f
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-07-20 14:19:05 +0200

    Completed basic structure for website

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,54 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import htmlhint from "eslint-plugin-htmlhint";
 
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:tailwindcss/recommended", "plugin:htmlhint/recommended" ],
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["html", "tailwindcss", "htmlhint"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "htmlhint/htmlhint": [
+      "error",
+      {
+        "tagname-lowercase": true,
+        "attr-lowercase": true,
+        "attr-value-double-quotes": true,
+        "doctype-first": true,
+        "tag-pair": true,
+        "spec-char-escape": true,
+        "id-unique": true,
+        "src-not-empty": true,
+        "attr-no-duplication": true,
+        "alt-require": true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.html"],
+      processor: "html/html-processor",
+    },
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".html"],
+      },
+    },
+  },
+  languageOptions: {
+    globals: globals.browser,
+  },
+};
